8e3c5791af5eab52167d1f47dd56f80075a3dc75
wangdengwu <dengwu.wang@gmail.com>
2021-07-01 15:22:06 +0800

    init project for home work

---
+++ src/test/java/org/example/todo/ToDoListTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListTestApplication.class, args);
+	}
+
+}
